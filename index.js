23978f6b26a3d8b3d11c4b4c24045ec43278505f
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-02-15 15:03:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,24 +8,17 @@ port = 3000;
 
 app.set('trust proxy', true);
 
-if (navigator.geolocation) {
+app.get("/", async (req, res) => {
+
+   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      console.log(Latitude= $(latitude), Longitude= $(longitude));
+      res.send(Latitude= $(latitude), Longitude= $(longitude));
     });
   } else {
-    console.log("Geolocation is not supported by this browser.");
-
-    
+    console.log("Geolocation is not supported by this browser."); 
   }
-
-
-
-app.get("/", async (req, res) => {
-    const ip = req.ip;
-    res.send(ip);
-    console.log(ip);
 
 });
 app.listen(port, () => {
